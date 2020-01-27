ead52f4ed7b851f556d65501bd168c6cc84ca727
Daniel Habenicht <daniel-habenicht@outlook.de>
2020-01-27 23:23:22 +0100

    fix: update task

--- src/freestyle/index.ts
+++ src/freestyle/index.ts
@@ -1,57 +1,25 @@
 import tl = require('azure-pipelines-task-lib/task');
 import tr = require('azure-pipelines-task-lib/toolrunner');
-import { Utility } from './utility/utility';
-import semanticRelease, { GlobalConfig } from 'semantic-release';
-import * as path from 'path';
 
 async function run() {
-  const localFolder = path.join(__dirname);
+  const cwd = tl.getInput('cwd');
 
-  const config = Utility.getConfig();
-
-  tl.debug(`Executing NPM Command in: ${localFolder}`);
+  tl.debug(`Executing NPM Command in: ${cwd}`);
   const npm = new tr.ToolRunner('npm');
-  npm.line(`install --only=prod ${Utility.getNpmPackagesFromConfig(config).join(' ')}`);
+  npm.line('run sentry -- ' + tl.getInput('command') || '--help');
   npm
     .exec({
-      cwd: localFolder
+      cwd: cwd
     } as any)
-    .then(async () => {
-      await runSemanticRelease(config);
+    .then(async success => {
+      tl.setResult(
+        tl.TaskResult.Succeeded,
+        `Succesfully run command`
+      );
+    }, async error => {
+      tl.setResult(tl.TaskResult.Failed, error.message);
     });
 }
 
-async function runSemanticRelease(config: GlobalConfig) {
-  try {
-    const result = await semanticRelease(
-      { ...config },
-      {
-        // Run semantic-release from `/path/to/git/repo/root` without having to change local process `cwd` with `process.chdir()`
-        cwd: tl.getInput('cwd'),
-        // Pass the variable `MY_ENV_VAR` to semantic-release without having to modify the local `process.env`
-        env: { ...process.env, ...Utility.getCredentials() },
-        stdout: process.stdout,
-        stderr: process.stderr
-      }
-    );
-
-    if (result) {
-      const { lastRelease, commits, nextRelease, releases } = result;
-
-      tl.setResult(
-        tl.TaskResult.Succeeded,
-        `Published ${nextRelease.type} release version ${nextRelease.version} containing ${commits.length} commits.`
-      );
-
-      for (const release of releases) {
-        console.log(`The release was published with plugin "${release.pluginName}".`);
-      }
-    } else {
-      tl.setResult(tl.TaskResult.Succeeded, 'No release published.');
-    }
-  } catch (err) {
-    tl.setResult(tl.TaskResult.Failed, err.message);
-  }
-}
 
 run();
